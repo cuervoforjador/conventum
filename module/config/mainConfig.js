3a38e0bc46a9d9631695dacf24d24862db18d915
cuervoforjador <martintonie@gmail.com>
2023-04-22 04:31:29 +0200

    Test

---
+++ module/config/mainConfig.js
@@ -0,0 +1,20 @@
+/**
+ * Config...
+ */
+
+export class mainConfig {
+
+    /**
+     * Init Extend Configuration ...
+     * @param {object} oConfig 
+     */
+    static init(oConfig) {
+        oConfig.version10 = ( Math.floor(Number(game.version)) === 10 );
+        oConfig.version11 = ( Math.floor(Number(game.version)) === 11 );
+    }
+
+    static _renderCompendiumDirectory(tab, element, info) {
+        HookCompendium._stylingLiCompendium(tab);
+    }
+
+}
--- module/hooks/_hooksCompendium.js
+++ module/hooks/_hooksCompendium.js
@@ -9,18 +9,63 @@ export class HookCompendium {
      */    
     static _stylingLiCompendium(tab) {
         
+<<<<<<< HEAD
         tab._element.find('li').each(function(i,e) {
             $(e).addClass('_custoLiCompendium');
         });
+=======
+        //Custom class
+        if ( Math.floor(Number(game.version)) === 11 ) {
+            // Version 11...
+            tab._element.find('li').each(function(i,e) {
+                $(e).addClass('_custoLiCompendium');    
+            });
+        } else {
+            // Version 10...
+            tab._element.find('li.compendium-type').each(function(i0,e0) {
+                $(e0).find('li.compendium-pack').each(function(i,e) {
+                    $(e).addClass('_custoLiCompendium');
+                    $(e).addClass('_v10');
+                    $(e).css({'background-image': 'url(/systems/conventum/image/content/compendium/banner'+
+                                                                                $(e).find('h4 a').text().trim()+'.png)'});
+                    
+                });
+            });
+        }
+>>>>>>> becf3bb (Test)
     }
 
     /**
      * Styling Compendium Window 
      * @param {object} compendium
      */    
+<<<<<<< HEAD
     static _stylingCompendium(compendium) {
         
         compendium._element.find('.header-banner').addClass('_custoHeaderCompendium');
+=======
+    static _stylingCompendium(compendium) {        
+        
+        // Version 11...
+        if ( Math.floor(Number(game.version)) === 11 ) {
+            compendium._element.find('.header-banner').addClass('_custoHeaderCompendium');
+        } else {
+            compendium._element.find('section.window-content').addClass('_v10');
+            compendium._element.find('.directory-header').addClass('_custoHeaderCompendium');
+            compendium._element.find('.directory-header').addClass('_v10');
+            compendium._element.find('.directory-header').prepend(
+                '<div class="header-banner" style="background-image: '+
+                    'url(/systems/conventum/image/content/compendium/banner'+
+                        compendium._element.find('h4.window-title').text().trim()+'.png)">'+
+                    '<h4 class="pack-title">'+
+                        compendium._element.find('h4.window-title').text().trim()+
+                    '</h4>'+
+                '</div>'
+            );
+
+            compendium._element.find('.header-banner').addClass('_v10');
+        }
+>>>>>>> becf3bb (Test)
     }
 
 }
